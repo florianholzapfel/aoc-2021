406c9267f0a933366d83bd4ea44c4d5358600127
Florian Holzapfel <florian.holzapfel@plainconcepts.com>
2021-12-02 07:53:06 +0100

    improve day one by using the integrated functions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,34 +1,10 @@
-fun <T> window(list: List<T>, size: Int): List<List<T>> {
-    val result = mutableListOf<List<T>>()
-    for (index in list.indices) {
-        if (index + size > list.size) {
-            break
-        }
-        result.add(list.subList(index, index + size))
-    }
-    return result
-}
-
 fun main() {
     fun part1(input: List<Int>): Int {
-        return input.foldIndexed(0) { idx, acc, element ->
-            if (idx > 0 && element > input[idx-1]) {
-                acc + 1
-            } else {
-                acc
-            }
-        }
+        return input.zipWithNext().count{(x, y) -> y > x}
     }
 
     fun part2(input: List<Int>): Int {
-        val list = window(input, 3)
-        return list.foldIndexed(0) { idx, acc, element ->
-            if (idx > 0 && element.sum() > list[idx-1].sum()) {
-                acc + 1
-            } else {
-                acc
-            }
-        }
+        return part1(input.windowed(3).map { it.sum() })
     }
 
     val input = readInput("Day01").map { it.toInt() }
